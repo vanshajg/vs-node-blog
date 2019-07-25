82e49b2043819178a95d66214b218b107de44ea1
Vanshaj Girotra <vanshaj.g@practo.com>
2019-07-25 22:07:02 +0530

    Downgrade eslint Issue with prettier on latest ESlint. Will Update as soon as that bug is fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // routing page
 console.log('hello world " testing " test 1234 tested vansh');
 
-require('dotenv').config(); //once used with .env module , process.env object will contain all data of .env config file.
+require('dotenv').config(); // once used with .env module , process.env object will contain all data of .env config file.
 
 console.log(process.env.DB_HOST);
